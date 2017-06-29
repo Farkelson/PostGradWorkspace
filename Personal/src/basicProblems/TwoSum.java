3a30d4a172930db23425809b619374df559c1684
Farkelson <scottmglass@gmail.com>
2017-06-28 17:46:48 -0700

    Worked on basic questions

---
+++ Personal/src/basicProblems/TwoSum.java
@@ -0,0 +1,14 @@
+package basicProblems;
+
+public class TwoSum {
+    public static int[] findTwoSum(int[] list, int sum) {
+    	int[] retArray = new int[2];
+    	
+        return retArray;
+    }
+
+    public static void main(String[] args) {
+        int[] indices = findTwoSum(new int[] { 1, 3, 5, 7, 9 }, 12);
+        System.out.println(indices[0] + " " + indices[1]);
+    }
+}
